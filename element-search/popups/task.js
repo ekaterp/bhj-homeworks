4807686efdef2bb416211e8ccc81e906f2e83b1e
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-10 03:14:46 +0400

    Task1_v2

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,26 +1,15 @@
-const modal_main = document.getElementsByClassName('modal');
-const modal_content = document.getElementsByClassName('modal__content')
+const modals = document.querySelectorAll(".modal");
+modals[0].classList.add("modal_active");
 
-modal_main[0].classList.add('modal_active');
-
-let modals = Array.from(modal_content);
-
-let buttonCloseMain = modals[0].getElementsByClassName('modal__close');
-let buttonCloseSuccess = modals[1].getElementsByClassName('modal__close');
-let buttonMain = modals[0].getElementsByClassName('btn_danger');
-
-buttonMain[0].addEventListener('click',() => {
-    modal_main[0].classList.remove("modal_active");
-    modal_main[1].classList.add("modal_active");
-});
-
-buttonCloseMain[0].addEventListener('click',() => modalClose(modal_main));
-buttonCloseSuccess[0].addEventListener('click',() => modalClose(modal_main));
-
-function modalClose(modal_main) {
-    if (modal_main[0].classList.contains("modal_active")) {
-        modal_main[0].classList.remove("modal_active");
-    } else if (modal_main[1].classList.contains("modal_active")) {
-        modal_main[1].classList.remove("modal_active");
-    }
+button = modals[0].querySelector(".btn_danger");
+button.onclick = () => {
+    modals[0].classList.remove("modal_active");
+    modals[1].classList.add("modal_active");
 }
+
+modals.forEach((modal) => {    
+    let closeElement = modal.getElementsByClassName('modal__close');
+    closeElement[0].onclick = () => {
+        modal.classList.remove("modal_active");
+    }
+})
