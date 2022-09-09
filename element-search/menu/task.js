049244518fe23aafdce79474d335a0423c0f518d
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-10 02:35:58 +0400

    Done Task2

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+const links = document.querySelectorAll(".menu__link");
+
+links.forEach((link) => {
+    link.onclick = function(event) {
+        const element = document.querySelector('.menu_active');
+        if (element) {
+            element.classList.remove('menu_active');
+        }
+        if (link.closest('.menu__item').querySelector('.menu_sub')) {
+            link.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active');
+            return false;
+        }
+    }
+    
+});
+
