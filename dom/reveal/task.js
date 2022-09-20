9a254ef28cc27a3920008357f60367e9686a5776
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-20 22:01:36 +0400

    Task1 refactoring

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,15 +1,27 @@
 const reveals = document.querySelectorAll('.reveal');
 
-document.addEventListener('scroll', () => {
+function throttle(callback, limit) {
+    let waiting = false;
+    return function () {
+        if (!waiting) {
+            callback.apply(this, arguments);
+            waiting = true;
+            setTimeout(function () {
+                waiting = false;
+            }, limit);
+        }
+    }
+}
+
+document.addEventListener('scroll', throttle(() => {
     reveals.forEach(reveal => {
         let viewPortHeight = window.innerHeight;
         let revealTop = reveal.getBoundingClientRect().top;
         let revealHeight = reveal.getBoundingClientRect().height
-        if ((revealTop > -revealHeight) && (revealTop < viewPortHeight)) {
+        if (revealTop > -revealHeight && revealTop < viewPortHeight) {
             reveal.classList.add("reveal_active");
         } else {
             reveal.classList.remove("reveal_active");
         }
-    });    
-}); 
-
+    });
+}, 50));
