7e73d68f72ba1be5ac35db45628c8a84482afeeb
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-23 20:55:59 +0400

    Refactoring Task2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,11 +3,17 @@ const links = document.querySelectorAll(".menu__link");
 links.forEach((link) => {
     link.onclick = function(event) {
         const element = document.querySelector('.menu_active');
+        const submenu = link.closest('.menu__item').querySelector('.menu_sub');
+        
         if (element) {
             element.classList.remove('menu_active');
+            if (submenu === element) {
+                return false;
+            }
         }
-        if (link.closest('.menu__item').querySelector('.menu_sub')) {
-            link.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active');
+        
+        if (submenu) {
+            submenu.classList.toggle('menu_active');
             return false;
         }
     }
