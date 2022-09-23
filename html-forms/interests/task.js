4bca2c3f8215431e38985045fe5afc3bbce62a5a
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-23 19:50:57 +0400

    Refactoring Task2

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,11 +3,7 @@ let checked = false;
 interests.forEach(interest => {
     interest.onchange = () => {
         const childInterests = interest.querySelector("ul").querySelectorAll(".interest__check");
-        if (checked) {
-            checked = false;
-        } else {
-            checked = true;
-        }
+        checked = !checked;
         childInterests.forEach(childInterest => {
             if (checked === false) {
                 childInterest.checked = false
