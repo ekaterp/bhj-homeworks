331be16fa61b52031df91a6e20240debd84901b4
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-24 01:28:19 +0400

    Done Task1

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,20 @@
+const haveTooltip = Array.from(document.querySelectorAll(".has-tooltip"));
+let indexLastTooltip = -1;
+
+haveTooltip.forEach((hasTooltip) => {
+    hasTooltip.onclick = (event) => {
+        event.preventDefault();
+        let positionTop = hasTooltip.offsetTop + 20;
+        const tooltip = '<div class="tooltip tooltip_active" style="data-position: bottom; position: absolute; margin-left: ' + hasTooltip.offsetLeft +'px; top: '+ positionTop +'px">' + hasTooltip.getAttribute("title") + '</div>';
+        const activeTooltip = document.querySelector(".tooltip_active");
+       
+        if (activeTooltip) {
+            activeTooltip.remove();
+            if (indexLastTooltip === haveTooltip.indexOf(hasTooltip)) {
+                return false;
+            }
+        }
+        hasTooltip.insertAdjacentHTML('afterBegin', tooltip);
+    };
+    indexLastTooltip = haveTooltip.indexOf(hasTooltip);
+});
