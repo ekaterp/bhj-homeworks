5404e25773413041541a7a866cb38a50c877d52c
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-09-24 19:35:03 +0400

    Done Task2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,19 +2,6 @@ const inputForm = document.forms;
 const tasksInput = document.querySelector(".tasks__input");
 const tasksList = document.getElementById("tasks__list");
 
-function addTask(message) {
-    let newTask = `
-    <div class="task">
-        <div class="task__title"> ${message} </div>
-        <a href="#" class="task__remove">&times;</a>
-    </div>`;
-    tasksList.insertAdjacentHTML("beforeend", newTask);
-    tasksList.lastElementChild.querySelector('.task__remove').addEventListener('click', function(e) {
-        e.preventDefault();
-        e.target.parentElement.remove();
-    });
-};
-
 inputForm[0].addEventListener('submit', event => {
     if (tasksInput.value.trim() != "") {
         event.preventDefault();
@@ -22,5 +9,38 @@ inputForm[0].addEventListener('submit', event => {
         tasksInput.value = "";
     }
 });
+
+function addTask(task) {
+    let newTask = `
+    <div class="task">
+        <div class="task__title"> ${task} </div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>`;
+
+    tasksList.insertAdjacentHTML("beforeend", newTask);
+    tasksList.lastElementChild.querySelector('.task__remove').addEventListener('click', function(e) {
+        e.preventDefault();
+        e.target.parentElement.remove();
+    });
+};
+
+function alternativeAddTask(task) {
+    let newTask = document.createElement('div');
+    newTask.classList.add('task');
+    let newTaskTitle = document.createElement('div');
+    newTaskTitle.classList.add('task__title');
+    newTaskTitle.innerText = task;
+    newTask.appendChild(newTaskTitle);
+    let newTaskRemoveLink = document.createElement('a');
+    newTaskRemoveLink.classList.add('task__remove');
+    newTaskRemoveLink.innerHTML = '&times;';
+    newTask.appendChild(newTaskRemoveLink);
+    newTaskRemoveLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        e.target.parentElement.remove();
+    });
+
+    tasksList.appendChild(newTask);
+};
 
 
