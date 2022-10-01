b3554c7308fbb34cd46ce56d75fe25f36de62e6e
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-10-01 13:13:40 +0400

    Task3_v2

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form'); 
+const form = document.getElementById('form');
 const progress = document.getElementById('progress');
 
 form.addEventListener('submit', (event) => {
@@ -6,31 +6,26 @@ form.addEventListener('submit', (event) => {
 
     const formData = new FormData(form);
     const url = form.getAttribute('action');
-    let progressRate = 0;
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
+    const fileSize = form.file.files[0].size;
 
-    if (xhr.upload) {
-
-        xhr.addEventListener('progress', function (event) {
-            let fileSize = form.file.files[0].size;
-            const progressRate = event.loaded / 100000000;
-            progress.setAttribute('value', progressRate);
-        });
-
-        xhr.addEventListener('readystatechange', () => {
-            if (xhr.readyState === xhr.DONE) {
-                progressRate = 1;
-                progress.setAttribute('value', progressRate);
-                if (xhr.status == 200) {
-                    alert('Данные успешно отправлены');
-                } else {
-                    console.error("HTTP request failed", xhr.status, xhr.responseText);
-                }
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status == 200) {
+                alert('Данные успешно отправлены');
+            } else {
+                console.error("HTTP request failed", xhr.status, xhr.responseText);
             }
-        });
-    
-        xhr.send(formData);
-    }
+        }
+    });
+
+    xhr.upload.addEventListener('progress', function (e) {
+        console.log('loaded ', e.total, ' bytes of ', fileSize);
+        progress.value = e.loaded / fileSize;
+    });
+
+    xhr.open(form.getAttribute('method'), form.getAttribute('action'));
+    xhr.send(formData);
+
 });
