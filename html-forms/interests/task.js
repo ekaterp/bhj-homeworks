7241bf2ac36228de12e1749b6a04b641de7c54fd
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-10-02 16:30:03 +0400

    Task2_v3

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,10 +3,19 @@ const interests = Array.from(document.querySelector("ul").children);
 interests.forEach(interest => {
     interest.addEventListener("change", (e) => {
         e.preventDefault();
-        e.target.closest('li').querySelectorAll('ul input[type=checkbox]').forEach( item => {
-            if (!item.checked) {
-                item.checked = e.target.checked;
+        let needInversion =  true;
+        const childCheckboxes = Array.from(e.target.closest('li').querySelectorAll('ul input[type=checkbox]'));
+        if (childCheckboxes.length > 0) {
+            for (let i=1; i < childCheckboxes.length; i++) {
+                if (childCheckboxes[i].checked === e.target.checked) {
+                    needInversion = false;
+                }
             }
-          })
+            if (needInversion) {
+                childCheckboxes.forEach( item => {
+                    item.checked = e.target.checked;
+              })
+            }
+        }
     })
 });
