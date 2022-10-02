66cb0cae9682cc0effb3b4b8af0248f1a1065e02
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-10-02 17:17:59 +0400

    Task1_v1

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,6 +2,5 @@ const textarea = document.querySelector('textarea');
 textarea.textContent = localStorage.getItem('textarea');;
 
 textarea.addEventListener('input', (e) => {
-    e.preventDefault();
     localStorage.setItem('textarea', document.querySelector('textarea').value);
 })
